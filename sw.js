83d3fd5e46b0454d285b20099bc2116cfcd68a7e
decobwebs <cobwebb784@gmail.com>
2025-10-11 00:22:32 +0100

    Favicon

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'quick-trips-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/css/style.css',
+  '/js/main.js',
+  // Add other assets you want to cache
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
